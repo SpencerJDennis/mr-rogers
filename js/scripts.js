419e4b6d6dd31f7c3f97d2dd1f2bc0b742bbcac7
Spencer Dennis <sjdennis2113@gmail.com>
2022-08-19 16:19:53 -0700

    add rogersArray toString: WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,26 @@
 const rogersInput = function(event) {
   event.preventDefault();
+  let rogers = "Here are your numbers: "
   let input1 = parseInt(document.getElementById("input1").value);
   let rogersArray = []
   console.log(input1);
   for (let index = 0; index <= input1; index++) {
-    console.log(rogersArray)
     rogersArray.push(index);
     console.log(rogersArray)
-    let newRogersArray = []
-    newRoggersArray.push(rogersArray)
-      console.log(newArray)
-      if (newRogersArray.index === 1) {
-        return "Beep!"
-      } else if (newRogersArray.index === 2 || newRogersArray.index === 1) {
-        return "Boop!"
-      } else if (newRogersArray.index === 3 || newRogersArray.indexd === 2 || newRogersArray.index === 1) {
-        return "Won't you be my neighbor?"
-      } else {
-        return newRogersArray
-       
-      }
-      
-    };
   };
+    rogers = rogersArray.concat(" " + rogersArray.toString([index]) + "!");
+    console.log(rogers)
+  };
+  /*let newArray = []
+  rogersArray.push(newArray)
+  console.log(newArray)
+  /*if (newArray.includes(2)) {
+    return replace(/2/, "bleep!")
+  }else {
+    return index
+  }
+  console.log(newArray)*/
+
 
 
   
